5eaad35af07d995aa32aca486f5d72d650e2515c
Ilya <EvilMav@users.noreply.github.com>
2020-12-11 01:37:42 +0100

    Shortcuts for windows build

--- cx_setup.py
+++ cx_setup.py
@@ -9,18 +9,40 @@ build_exe_options = {
     "excludes": ["tkinter", "PyQt5", "PyQt4", "pyqtgraph.examples"],
 }
 
+
+shortcut_table = [
+    (
+        "DesktopShortcut",  # Shortcut
+        "DesktopFolder",  # Directory_
+        "Gruseloskop",  # Name
+        "TARGETDIR",  # Component_
+        "[TARGETDIR]gruseloskop.exe",  # Target
+        None,  # Arguments
+        None,  # Description
+        None,  # Hotkey
+        None,  # Icon
+        None,  # IconIndex
+        None,  # ShowCmd
+        "TARGETDIR",  # WkDir
+    ),
+]
+
+msi_data = {"Shortcut": shortcut_table}
+bdist_msi_options = {"data": msi_data}
+
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 cx.setup(
     **setup_args,
-    options={"build_exe": build_exe_options},
+    options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
     executables=[
         cx.Executable(
             "bin/gruseloskop",
             targetName="gruseloskop.exe",
             shortcutName="Gruseloskop",
+            shortcutDir="StartMenuFolder",
             base=base,
         )
     ]
